80b4c0c4f1b0f0db00b2c7d5b3f08bbe1d7a6fe0
EAGDEV200 <127441932+EAGDEV200@users.noreply.github.com>
2023-06-20 20:42:10 -0300

    Revert "Backend (#1)" (#5)
    This reverts commit f9fd980af0010cf5819df83b861606852a14cb55.

--- models/commentModel.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const commentSchema = new mongoose.Schema({
-    codigo: Number,
-    comentario: String,
-    user: { type: mongoose.Schema.Types.ObjectId, ref: 'users' }
-});
-
-module.exports = mongoose.model('comments', commentSchema);
